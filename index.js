1c8d948beada4dedd1d36b9ec6e9b539c709475a
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-13 22:36:34 +1000

    1

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://blog.photofeeler.com/wp-content/uploads/2017/09/tinder-photo-size-tinder-picture-size-tinder-aspect-ratio-image-dimensions-crop.jpg',
+
+        }
+    },
+})
+
+
+ app.mount('#app')
