59c3cbcb12270d216b4d8e30ce5ad01a00287d63
AndrewSheu <andrewsheu0617@gmail.com>
2022-03-14 17:05:07 +1000

    finish

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const app = Vue.createApp({
             lastName: 'Doe',
             email: 'john@gmail.com',
             gender: 'male',
-            picture: 'https://blog.photofeeler.com/wp-content/uploads/2017/09/tinder-photo-size-tinder-picture-size-tinder-aspect-ratio-image-dimensions-crop.jpg',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
 
         }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch("https://randomuser.me/api")
+            const { results } = await res.json()
+
+            console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        },
     },
 })
 
